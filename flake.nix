c8119327281a4666bc70cf06462325fabfc34f15
Manu [tennox] <148720-TeNNoX@users.noreply.gitlab.com>
2023-02-28 18:44:16 +0100

    👷 [CI] pipeline that builds all templates as a test
    The build will fail because the devshell.overlay is deprecated,
    but I want to write a broken test first

--- flake.nix
+++ flake.nix
@@ -4,20 +4,37 @@
   inputs = {
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, flake-utils, devshell, nixpkgs }:
+  outputs = { self, flake-utils, devshell, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system: {
       devShell =
         let
           pkgs = import nixpkgs {
             inherit system;
 
-            overlays = [ devshell.overlay ];
+            overlays = [
+              (import rust-overlay)
+              devshell.overlays.default
+            ];
           };
+          rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" ];
+            # targets = [ "wasm-unknown-unknown" ];
+          });
         in
         pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          devshell.packages = with pkgs; [
+            # TODO rust-bin.fromRustupToolchainFile ./rust-toolchain
+            rust
+            nixpkgs-fmt
+          ];
+
+          # ? Not sure if needed
+          env = /* nixpkgs.lib.trace "${rust}/lib/rustlib/src/rust/library" */ [
+            { name = "RUST_SRC_PATH"; value = "${rust}/lib/rustlib/src/rust/library"; }
+          ];
         };
     });
 }
