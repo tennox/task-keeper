211f10a2002bcdda9e3ae590345188536609ebfe
Manu [tennox] <tennox+git@txlab.io>
2023-06-09 10:17:00 +0100

    devenv: WIP test & disable broken building

--- devenv.nix
+++ devenv.nix
@@ -3,17 +3,26 @@
 { pkgs, ... }:
 {
 
-  languages.rust = { # https://github.com/cachix/devenv/blob/main/src/modules/languages/rust.nix
+  languages.rust = {
+    # https://github.com/cachix/devenv/blob/main/src/modules/languages/rust.nix
     enable = true;
     version = "latest"; # = nightly
   };
+  languages.java.enable = true;
 
   packages = with pkgs; [
-    
+    gnumake
+    deno
+    sbt
+    go-task
+    nodejs
+    python3
+    cargo-make
+    # adoptopenjdk-jre-bin
   ];
 
-  #env.VAR = ""; 
-  #enterShell = ''
-  #   echo $VAR
-  #'';
+  env = {
+    NODE_HOME = "${pkgs.nodejs}";
+    # JAVA_HOME = "${pkgs.adoptopenjdk-jre-bin.home}";
+  };
 }
